172913f4c822e2256f88bb8b113fbe269609371e
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-01-14 23:09:12 -0500

    Migrate everything to use Tweego

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-TWEE="node twee3/src/index.js"
-
-FORMAT="snowman-2.0.2"
+TWEE="tweego/tweego"
+OUTDIR=build/
 
 mkdir -p build
-$TWEE build a-warm-place-to-stay/warm.twee build/a-warm-place-to-stay.html $FORMAT
-$TWEE build life-of-the-party/life.twee build/life-of-the-party.html $FORMAT
+$TWEE a-warm-place-to-stay -o $OUTDIR/"A Warm Place to Stay.html"
+$TWEE life-of-the-party -o $OUTDIR/"Life of the Party.html"
