ce002d9931cb6690fa21e148fd3c8347d63dfdd9
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-01-15 12:43:22 -0500

    Update README and build steps

--- build.fish
+++ build.fish
@@ -1,20 +1,29 @@
 #!/usr/bin/env fish
 
+# Generates builds of stories
+
 set -l TWEE 'tweego/tweego'
 set -l OUTDIR 'build/'
 
 set -l DIRS 'a-warm-place-to-stay' 'life-of-the-party'
+
+set -l PROOF_FORMATS 'illume' 'poof' 'dotgraph'
 
 # execute with -w [source] flag to watch a directory
 argparse -i 'i/input=' -- $argv
 
 
 mkdir -p $OUTDIR
+mkdir -p $OUTDIR/proof
 
 if test $_flag_i;
     $TWEE $_flag_i -o $OUTDIR/$_flag_i.html $argv
 else
   for dir in $DIRS
     $TWEE $dir -o $OUTDIR/$dir.html
+    for f in $PROOF_FORMATS
+      $TWEE -f $f $dir -o $OUTDIR/proof/$dir-$f.html
+    end
   end
 end
+
