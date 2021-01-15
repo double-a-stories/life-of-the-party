223e78f5df122463208a24b00861637ad5b97428
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-01-15 12:43:22 -0500

    Update README and build steps

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-TWEE="tweego/tweego"
-OUTDIR=build/
-
-mkdir -p build
-$TWEE a-warm-place-to-stay -o $OUTDIR/"A Warm Place to Stay.html"
-$TWEE life-of-the-party -o $OUTDIR/"Life of the Party.html"
