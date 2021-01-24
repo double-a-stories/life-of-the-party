a1d352cbdfb8a2a7fab06da31b5d1abd76307786
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-01-24 18:54:29 -0500

    Build script generates index.html in root

--- build.fish
+++ build.fish
@@ -11,7 +11,7 @@ set -l PROOF_FORMATS 'illume' 'poof' 'dotgraph'
 
 mkdir -p $OUTDIR
 
-$TWEE ./src/ -o $OUTDIR/$NAME.html --log-files $argv
+$TWEE ./src/ -o index.html --log-files $argv
 
 and for f in $PROOF_FORMATS
     $TWEE ./src/ -f $f -o $OUTDIR/$f-$NAME.html $argv
