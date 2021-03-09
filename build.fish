3415039b88f54ea1f9d8320b788d72a99a15de60
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-03-09 16:53:13 -0500

    Remove build files from repo

--- build.fish
+++ build.fish
@@ -13,8 +13,6 @@ mkdir -p $OUTDIR
 
 $TWEE ./src/ -o $OUTDIR/$NAME.html --log-files $argv
 
-and cp $OUTDIR/$NAME.html index.html
-
 and for f in $PROOF_FORMATS
     $TWEE ./src/ -f $f -o $OUTDIR/$f-$NAME.html $argv
 end
