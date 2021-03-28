16d498a7b9c38c0582f42c95a1d15e75d53e4f89
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-03-28 16:16:31 -0400

    setup.version is now story.version

--- src/menu/scripts.js
+++ src/menu/scripts.js
@@ -2,7 +2,7 @@
 
 // Global Variable: "setup"
 window.setup = window.setup || {};
-setup.version = "v0.5.0-indev"
+story.version = "v0.5.0-indev"
 
 // Move story styles to before the passages.
 // This prioritizes styles declared inside passages.
