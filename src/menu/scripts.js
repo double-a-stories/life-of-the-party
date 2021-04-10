13a2da8823318a9da8a1eab3df01e2c5aa159c4a
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-04-10 13:41:29 -0400

    Update license and thread URL

--- src/menu/scripts.js
+++ src/menu/scripts.js
@@ -3,7 +3,7 @@
 // Global Variable: "setup"
 window.setup = window.setup || {};
 story.version = "v0.5.0-indev"
-story.mainurl = "https://aryion.com/forum/viewtopic.php?f=79&t=59177"
+story.mainURL = "https://aryion.com/forum/viewtopic.php?f=79&t=59177"
 
 // Move story styles to before the passages.
 // This prioritizes styles declared inside passages.
