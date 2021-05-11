7e004be218d306328c61b4123682c2c37143256a
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-05-11 16:14:44 -0400

    Refactor page fade script

--- src/twee/menu/fade.js
+++ src/twee/menu/fade.js
@@ -3,20 +3,10 @@
 // Passages can disable this by adding the tag "no_fade"
 
 $(window).on('sm.passage.hidden', () => {
-  story.$passageEl.css({ opacity: 0 });
+  story.$passageEl.fadeTo(0, 0);
 })
 
 $(window).on('sm.passage.shown', (ev, { passage }) => {
-  if (!passage.tags.some(tag => tag === "no_fade")) {
-    story.$passageEl.css({
-      opacity: 0
-    }).animate({
-      opacity: 1
-    }, {
-      duration: "slow",
-      queue: false
-    });
-  } else {
-    story.$passageEl.css({ opacity: 1 });
-  }
+  const noFade = passage.tags.some(tag => tag === "no_fade");
+  story.$passageEl.fadeTo(noFade ? 0 : "slow", 1);
 })
