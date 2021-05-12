c94aac0e18082b1685d2a5d6253e01d6d145bc32
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-05-11 21:41:26 -0400

    Restore working fade script logic

--- src/twee/menu/fade.js
+++ src/twee/menu/fade.js
@@ -3,10 +3,10 @@
 // Passages can disable this by adding the tag "no_fade"
 
 $(window).on('sm.passage.hidden', () => {
-  story.$passageEl.fadeTo(0, 0);
+  story.$passageEl.css({ opacity: 0 });
 })
 
 $(window).on('sm.passage.shown', (ev, { passage }) => {
   const noFade = passage.tags.some(tag => tag === "no_fade");
-  story.$passageEl.fadeTo(noFade ? 0 : "slow", 1);
+  story.$passageEl.animate({ opacity: 1 }, { duration: noFade ? 0 : "slow", queue: false });
 })
