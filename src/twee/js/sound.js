1fbbb6b42eda70a57dfa91e5ddde9830f28ac3b4
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-06-25 15:52:53 -0400

    Add outdoor ambience

--- src/twee/js/sound.js
+++ src/twee/js/sound.js
@@ -4,18 +4,21 @@ Simple sound controller using howler.js
 class SoundInstance {
   constructor(howlObject) {
     this.howl = howlObject;
+    howlObject.mute(true);
     this.id = howlObject.play();
     howlObject.pause(this.id);
+    howlObject.mute(false);
   }
   fadeIn(duration = 1000) {
     if (!this.howl.playing(this.id)) {
       this.howl.play(this.id);
-      this.howl.fade(0.0, 1.0, duration, this.id);
+      this.howl.loop(true, this.id);
+      this.howl.fade(0.0, this.howl.volume(), duration, this.id);
     }
   }
   fadeOut(duration = 1000) {
     if (this.howl.playing(this.id)) {
-      this.howl.fade(1.0, 0.0, duration, this.id);
+      this.howl.fade(this.howl.volume(), 0.0, duration, this.id);
       this.howl.once("fade", () => this.howl.stop(this.id), this.id);
     }
   }
@@ -38,22 +41,26 @@ const passageAudio = (howlObject, tags) => {
 
 const birds = new Howl({
   src: ["assets/sound/birds.mp3"],
-  html5: true,
   loop: true,
   volume: 0.6,
 });
 const gurgle = new Howl({
   src: ["assets/sound/gurgle.mp3"],
-  html5: true,
   loop: true,
   volume: 1.0
+});
+const crickets = new Howl({
+  src: ["assets/sound/crickets.mp3"],
+  loop: true,
+  volume: 0.1,
 });
 const epilogueMusic = new Howl({
   src: ["assets/sound/kevp888 - CD_YIN_001.mp3"],
   html5: true,
-  loop: true,
   volume: 1.0
 });
 passageAudio(birds, ["morning"]);
 passageAudio(gurgle, ["vore"]);
-passageAudio(epilogueMusic, ["epilogue"]);
+passageAudio(crickets, ["epilogue"]);
+
+passageAudio(epilogueMusic, ["epilogue", "stars"]);
