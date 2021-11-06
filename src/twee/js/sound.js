d9ee2b4cd77e805dd8cd8eb2eaf73bb0d4fa432d
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2021-11-06 17:31:41 -0400

    Optimize asset loading and caching...
    commit f53d399c9cca42e46bd0298599b3bb9299441c60
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 15:29:48 2021 -0400
    
        Defer howler and twemoji scripts
    
    commit 22f866bbb0a16960105bc003d236942b098a32b7
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 15:28:07 2021 -0400
    
        Add preload <link> for title screen assets
    
        Bake the ⚠️ graphic into the title screen.
    
    commit bc9eb77bc7c00023ef0c496004831da4b21d69a2
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 14:32:49 2021 -0400
    
        Add explicit dimensions to horse icons
    
    commit e9fd9054cecdb39ca6a0e917a945a879fc8f39c2
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 14:32:25 2021 -0400
    
        Store SVGs externally
    
    commit b22f357071d8c2ce72d9bff2bcbcf73864e18ac0
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 14:03:12 2021 -0400
    
        Bundle vendor JS (howler, twemoji) in assets
    
    commit f7705a083f7f467e1f7dd7765ab3c9fb1c007fbb
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 14:02:19 2021 -0400
    
        Bundle fonts in assets, rather than index.html
    
    commit 7e81a72184539a6490c024deaaea83b5792cef95
    Author: double-a-stories <50436716+double-a-stories@users.noreply.github.com>
    Date:   Fri Nov 5 13:49:35 2021 -0400
    
        Defer loading sound files until actually needed.

--- src/twee/js/sound.js
+++ src/twee/js/sound.js
@@ -1,111 +1,124 @@
-// Mute and unmute on load.
-$(document).on("sm.passage.shown", () => {
-  if (!setup.isFlagSet("enableSound")) {
-    Howler.mute(true);
-  } else {
-    Howler.mute(false);
+let howlerInitialized = false;
+$(document).on("sm.story.started sm.passage.showing", () => {
+  if (!howlerInitialized && window.Howl) {
+    howlerInit();
+    howlerInitialized = true;
+    console.info("Howler.js was loaded.")
   }
 });
 
-/*
-Simple sound controller using howler.js
-*/
-class SoundInstance {
+const howlerInit = () => {
+
+  // Mute and unmute on load.
+  $(document).on("sm.passage.shown", () => {
+    if (!setup.isFlagSet("enableSound")) {
+      Howler.mute(true);
+    } else {
+      Howler.mute(false);
+    }
+  });
+
+  /*
+  Simple sound controller using howler.js
+  */
+  class SoundInstance {
+    /**
+     *
+     * @param {Howl} howlObject
+     */
+    constructor(howlObject) {
+      this.howl = howlObject;
+      this.playing = false;
+    }
+    fadeIn(duration = 1000) {
+      // If sound isn't playing yet, then fade in.
+      if (this.playing) {
+        return;
+      }
+      if (this.id == undefined) {
+        this.howl.load();
+        this.id = this.howl.play();
+      } else {
+        this.howl.play(this.id);
+      }
+      this.howl.fade(0.0, this.howl.volume(), duration, this.id);
+      this.playing = true;
+      this.howl.off("fade", this.id);
+    }
+    fadeOut(duration = 1000) {
+      if (this.howl.playing(this.id)) {
+        this.howl.fade(this.howl.volume(), 0.0, duration, this.id);
+        this.playing = false;
+        this.howl.once("fade", () => {
+          this.playing = false;
+          this.howl.pause(this.id);
+        }, this.id);
+      }
+    }
+  }
+
+  // Ambient bird noises for morning endings
+  const audioRootPath = "assets/sound/";
+
   /**
    *
    * @param {Howl} howlObject
+   * @param  {...((passage: Passage) => boolean)} predicates
+   * @returns {number}
    */
-  constructor(howlObject) {
-    this.howl = howlObject;
-    this.playing = false;
+  const passageAudio = (howlObject, ...predicates) => {
+    const soundInstance = new SoundInstance(howlObject);
+    $(window).on("sm.passage.shown", (ev, { passage }) => {
+      if (predicates.some(f => f(passage))) {
+        soundInstance.fadeIn();
+      } else {
+        soundInstance.fadeOut();
+      }
+    })
+    return soundInstance.id;
   }
-  fadeIn(duration = 1000) {
-    // If sound isn't playing yet, then fade in.
-    if (this.playing) {
-      return;
-    }
-    if (this.id == undefined) {
-      this.id = this.howl.play();
-    } else {
-      this.howl.play(this.id);
-    }
-    this.howl.fade(0.0, this.howl.volume(), duration, this.id);
-    this.playing = true;
-    this.howl.off("fade", this.id);
+
+  /**
+   *
+   * @param  {...any} tags
+   * @returns {(passage: Passage) => boolean}
+   */
+  const hasTags = (...tags) => (passage) => tags.every(t => passage.tags.includes(t));
+
+  const howlDefaults = {
+    loop: true,
+    volume: 0.1,
+    autoplay: false,
+    preload: false
   }
-  fadeOut(duration = 1000) {
-    if (this.howl.playing(this.id)) {
-      this.howl.fade(this.howl.volume(), 0.0, duration, this.id);
-      this.playing = false;
-      this.howl.once("fade", () => {
-        this.playing = false;
-        this.howl.pause(this.id);
-      }, this.id);
-    }
-  }
+  // Chirping birds from freesound
+  // Used in the "morning after" passages
+  const birds = new Howl({
+    ...howlDefaults,
+    src: ["assets/sound/birds.mp3"],
+    volume: 0.05,
+  });
+  passageAudio(birds, hasTags("morning"));
+  // Stomach sounds from freesound
+  const gurgle = new Howl({
+    ...howlDefaults,
+    src: ["assets/sound/gurgle.mp3"],
+    volume: 1.0,
+  });
+  passageAudio(gurgle, hasTags("vore"));
+  // Crickets and suburban outdoor noises from freesound
+  const crickets = new Howl({
+    ...howlDefaults,
+    src: ["assets/sound/crickets.mp3"],
+    volume: 0.1,
+  });
+  passageAudio(crickets, hasTags("outside"));
+  // Ambient noise by CD_YIN_001.mp3 by kevp888
+  // Used in the zen scene
+  const zenMusic = new Howl({
+    ...howlDefaults,
+    src: ["assets/sound/kevp888 - CD_YIN_001.mp3"],
+    volume: 1.0,
+  });
+  passageAudio(zenMusic, hasTags("zen"));
 }
-
-// Ambient bird noises for morning endings
-const audioRootPath = "assets/sound/";
-
-/**
- *
- * @param {Howl} howlObject
- * @param  {...((passage: Passage) => boolean)} predicates
- * @returns {number}
- */
-const passageAudio = (howlObject, ...predicates) => {
-  const soundInstance = new SoundInstance(howlObject);
-  $(window).on("sm.passage.shown", (ev, { passage }) => {
-    if (predicates.some(f => f(passage))) {
-      soundInstance.fadeIn();
-    } else {
-      soundInstance.fadeOut();
-    }
-  })
-  return soundInstance.id;
-}
-
-/**
- *
- * @param  {...any} tags
- * @returns {(passage: Passage) => boolean}
- */
-const hasTags = (...tags) => (passage) => tags.every(t => passage.tags.includes(t));
-
-const howlDefaults = {
-  loop: true,
-  volume: 0.1,
-  autoplay: false,
-  preload: 'metadata'
-}
-// Chirping birds from freesound
-// Used in the "morning after" passages
-const birds = new Howl({
-  ...howlDefaults,
-  src: ["assets/sound/birds.mp3"],
-  volume: 0.05,
-});
-passageAudio(birds, hasTags("morning"));
-// Stomach sounds from freesound
-const gurgle = new Howl({
-  ...howlDefaults,
-  src: ["assets/sound/gurgle.mp3"],
-  volume: 1.0,
-});
-passageAudio(gurgle, hasTags("vore"));
-// Crickets and suburban outdoor noises from freesound
-const crickets = new Howl({
-  ...howlDefaults,
-  src: ["assets/sound/crickets.mp3"],
-  volume: 0.1,
-});
-passageAudio(crickets, hasTags("outside"));
-// Ambient noise by CD_YIN_001.mp3 by kevp888
-// Used in the zen scene
-const zenMusic = new Howl({
-  ...howlDefaults,
-  src: ["assets/sound/kevp888 - CD_YIN_001.mp3"],
-  volume: 1.0,
-});
-passageAudio(zenMusic, hasTags("zen"));
