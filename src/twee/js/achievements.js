ff2ba0c07c27c0a98b2bdb6847ea2a0c143708a5
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2022-02-07 15:36:06 -0500

    Refactor achievements handling code
    - Story flags now behave like a hashset
    - Moved achievement sorting code into achievements.js
    - Reordered the closet achievement

--- src/twee/js/achievements.js
+++ src/twee/js/achievements.js
@@ -1,7 +1,9 @@
 /**
  * achievements.js
+ *
+ * A plugin for "Life of the Party". Manages story flags which are preserved
+ * between sessions.
  * Provides an API for persistent browser storage, using Lockr
- * Author: Double-A <https://github.com/double-a-stories>
  * License: MIT-0
  */
 
@@ -10,20 +12,53 @@
  */
 window.setup = window.setup || {};
 
+if (window.Lockr == undefined) {
+  // make sure Lockr is loaded.
+  throw new Error("achievements.js: Lockr.js was not loaded in time!");
+}
+
 // Savegame format version
 // Can be updated to introduce breaking changes with previous achievement storage formats.
-const ACHIEVEMENT_KEY_VERSION = 8;
+setup.ACHIEVEMENT_KEY_VERSION = 8;
 
-// This object stores valid achievement names. Create it if it isn't already defined.
+/**
+ * These achievements are used in Life of the Party
+ * Lotp Achievements are stored like [emoji, location, title, description]
+ */
 setup.ALL_ACHIEVEMENTS = setup.ALL_ACHIEVEMENTS || {};
 
 // Lockr key is the name of the story, plus the savegame version.
-Lockr.prefix = (`${story.name} ${ACHIEVEMENT_KEY_VERSION} `).replace(/\s/g, "_");
+Lockr.prefix = `${story.name} ${setup.ACHIEVEMENT_KEY_VERSION} `.replace(
+  /\s/g,
+  "_"
+);
 
 /**
  * @returns {string[]} The list of achievements which are currently unlocked.
  */
 setup.getAchievements = () => Lockr.smembers("achievements");
+/**
+ * @returns {string[]} The list of all achievements, with unlocked ones first.
+ */
+setup.getAllAchievementsSorted = () => {
+  return [
+    ...Object.keys(setup.ALL_ACHIEVEMENTS).filter((k) =>
+      setup.hasAchievement(k)
+    ),
+    ...Object.keys(setup.ALL_ACHIEVEMENTS).filter(
+      (k) => !setup.hasAchievement(k)
+    ),
+  ];
+};
+/**
+ * @returns {[number, number]} The number of active achievements, and the total.
+ */
+setup.getAchievementCount = () => {
+  return [
+    setup.getAchievements().length,
+    Object.keys(setup.ALL_ACHIEVEMENTS).length,
+  ];
+};
 /**
  * @param {string} name The name of a valid achievement.
  * @throws If the name is not a defined achievement.
@@ -39,24 +74,23 @@ setup.addAchievement = (name) => {
  * @param {string} name The name of an achievement
  * @returns Whether the given achievement has been acquired.
  */
-setup.hasAchievement = (name) =>
-  Lockr.sismember("achievements", name);
+setup.hasAchievement = (name) => Lockr.sismember("achievements", name);
 
 setup.addAllAchievements = () => {
   Object.keys(setup.ALL_ACHIEVEMENTS).forEach(setup.addAchievement);
   story.show(passage.id);
-}
+};
 setup.resetAchievements = () => Lockr.set("achievements", []);
 
 setup.resetAchievementsPrompt = function () {
-  const message = `This will reset your save file, including the ${setup.getAchievements().length} achievement(s) you have acquired so far.
-
-Are you sure you want to continue?`;
+  const message = `This will reset your save file, including the ${
+    setup.getAchievements().length
+  } achievement(s) you have acquired so far.\n\nAre you sure you want to continue?`;
   if (window.confirm(message)) {
     Lockr.flush();
     setup.restart();
   }
-}
+};
 
 /*
  * Story Flags API. These are represented as a hashset on an object.
@@ -67,41 +101,41 @@ Are you sure you want to continue?`;
  */
 setup.getFlags = () => Lockr.get("flags", {});
 /**
- * 
+ *
  * @param {string} name The flag to check
- * @returns 
+ * @returns
  */
 // Returns whether a flag is set to a non-zero value
 setup.isFlagSet = (name) => {
   return !!Lockr.get("flags", {})[name];
-}
+};
 /** Gets the count of a flag
- * @param {string} name The name of the flag to check 
+ * @param {string} name The name of the flag to check
  * @returns {number} The number of times the flag was set
  */
 setup.getFlag = (name) => {
   let flags = Lockr.get("flags", {});
-  return +(flags[name]) | 0;
-}
+  return +flags[name] | 0;
+};
 /**
  * @param {string} name The flag to set
  * @param {number} count The number to set it to (integer >= 0)
  */
 setup.setFlag = (name, count = 1) => {
-  let flags = Lockr.get("flags");
-  flags[name] = Math.max(0, +count|0); // increment
+  let flags = Lockr.get("flags", {});
+  flags[name] = Math.max(0, +count | 0); // increment
   Lockr.set("flags", flags);
-}
-/** 
+};
+/**
  * Adds a flag with the given name to the set of flags.
  * @param {string} name The flag to increment
  * @param {number} amt An integer number to add/subtract
  */
 setup.addFlag = (name, amt = 1) => {
-  let count = setup.getFlag(name)
-  count = Math.max(0, count + +amt|0); // increment
+  let count = setup.getFlag(name);
+  count = Math.max(0, (count + +amt) | 0); // increment
   setup.setFlag(name, count);
-}
+};
 /**
  * Unsets a flag
  * @param {string} name The flag to unset
@@ -110,7 +144,7 @@ setup.unsetFlag = (name) => {
   let flags = Lockr.get("flags");
   flags[name] = undefined;
   Lockr.set("flags", flags);
-}
+};
 /**
  * Unsets all flags.
  */
