93174121642281a6d8623c05567870e0fab5065e
double-a-stories <50436716+double-a-stories@users.noreply.github.com>
2022-03-29 17:39:55 -0400

    [engine] Create Templates for Hazel social media

--- src/twee/js/templates.js
+++ src/twee/js/templates.js
@@ -16,10 +16,54 @@ const Templates = (window.Templates = window.T = {});
       .join(" ") +
     `</p>`;
 
+  // Templates for Hazel Epilogue
+
+  const socialMediaComment = ([username, comment]) =>
+    `<p><b>${username}</b> ${comment}</p>`;
+
+  const hashtag = (tag) => `<span aria-label="hashtag">#</span>${tag}`;
+
+  const likes = (likes) =>
+    `<p class="social-card__likes"><span aria-label="${+likes | 0} likes">♡ ${
+      +likes | 0
+    }</span></p>`;
+
+  const hoursAgo = (hours) =>
+    `${+hours | 0}<span aria-label="hours">h</span> ago`;
+  const minutesAgo = (mins) =>
+    `${+mins | 0}<span aria-label="minutes">m</span> ago`;
+
+  const socialMediaCard = ({
+    name,
+    username,
+    time,
+    body,
+    hashtags,
+    likeCount,
+    comments,
+  }) =>
+    `<div class="social-card">
+    <p>${name} <small>${username} – ${time}</small></p>
+    <div class="social-card__body">${body}</div>
+    <p><small>${(hashtags || []).map(hashtag).join(" ")}</small>
+    ${likes(likeCount)}
+    ${
+      comments
+        ? `<div.social-card__comments>${(comments || [])
+            .map(socialMediaComment)
+            .join("\n")}</div>`
+        : ``
+    }
+</div>`;
+
   Object.assign(window.Templates, {
     CWI: contentWarningInline,
     contentWarningInline,
     CWP: contentWarningParagraph,
     contentWarningParagraph,
+    hoursAgo,
+    hashtag,
+    minutesAgo,
+    socialMediaCard,
   });
 }
