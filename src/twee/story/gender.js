93c9a42942ad0cb18bbefca59505b57a2012d916
Double-A <aa.vore@pm.me>
2024-08-26 01:26:07 -0400

    [story] Move R63 definitions into own file

---
+++ src/twee/story/gender.js
@@ -0,0 +1,16 @@
+// this file defines gender toggles for each character
+
+Rule63.define("Ana", Rule63.Gender.Female);
+Rule63.define("Basil", Rule63.Gender.Male);
+Rule63.define("Byron", Rule63.Gender.Male);
+Rule63.define("Clif", Rule63.Gender.Male);
+Rule63.define("Hazel", Rule63.Gender.Female);
+Rule63.define("Ivy", Rule63.Gender.Female);
+Rule63.define("Jackie", Rule63.Gender.Female);
+Rule63.define("Lacey", Rule63.Gender.Female);
+Rule63.define("Nikki", Rule63.Gender.Female);
+Rule63.define("Nox", Nikki ? Rule63.Gender.Female : Rule63.Gender.Male, true);
+Rule63.define("Reese", Rule63.Gender.Male);
+Rule63.define("Ren", Rule63.Gender.Male);
+window.Faith = false;
+window.Kaia = false; // not fully implemented
