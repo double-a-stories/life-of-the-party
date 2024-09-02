7c66021bf3c71572a02d0054543c9dce073ebcc5
Double-A <aa.vore@pm.me>
2024-09-02 00:07:42 -0400

    [sound] Refactor audio; add dubstep and party noises
    commit dd340a19db9efa8a98a147a104477c36fefb144b
    Author: Internet Hyena <10764601-mozzierella@users.noreply.gitlab.com>
    Date:   Fri Apr 26 15:45:09 2024 -0400
    
        [css] Zending: fix a graphical bug on the fadeout
    
    commit d0117a50239b2ea054154dd9d4484fdf95c957e6
    Author: Internet Hyena <10764601-mozzierella@users.noreply.gitlab.com>
    Date:   Fri Apr 26 13:42:23 2024 -0400
    
        [assets] Refactor audio; add dubstep and party noises

--- src/twee/js/sound.js
+++ src/twee/js/sound.js
@@ -1,134 +1,151 @@
-let howlerInitialized = false;
-$(document).on("sm.story.started sm.passage.showing", () => {
-  if (!howlerInitialized && window.Howl) {
-    howlerInit();
-    howlerInitialized = true;
-    console.info("Howler.js was loaded.")
+/**
+ * This module provides a quick and dirty API for controlling music.
+ * In hindsight this was kind of stupid.
+ */
+// Mute and unmute on load.
+$(document).on("sm.passage.shown", () => {
+  if (!window.StoryFlags.isSet("enableSound")) {
+    Howler.mute(true);
+  } else {
+    Howler.mute(false);
   }
 });
 
-const howlerInit = () => {
-
-  // Mute and unmute on load.
-  $(document).on("sm.passage.shown", () => {
-    if (!StoryFlags.isSet("enableSound")) {
-      Howler.mute(true);
-    } else {
-      Howler.mute(false);
-    }
-  });
-
-  /*
-  Simple sound controller using howler.js
-  */
-  class SoundInstance {
-    /**
-     *
-     * @param {Howl} howlObject
-     */
-    constructor(howlObject) {
-      this.howl = howlObject;
-      this.playing = false;
-    }
-    fadeIn(duration = 1000) {
-      // If sound isn't playing yet, then fade in.
-      if (this.playing) {
-        return;
-      }
-      if (this.id == undefined) {
-        this.howl.load();
-        this.id = this.howl.play();
-      } else {
-        this.howl.play(this.id);
-      }
-      this.howl.fade(0.0, this.howl.volume(), duration, this.id);
-      this.playing = true;
-      this.howl.off("fade", this.id);
-    }
-    fadeOut(duration = 1000) {
-      if (this.howl.playing(this.id)) {
-        this.howl.fade(this.howl.volume(), 0.0, duration, this.id);
-        this.playing = false;
-        this.howl.once("fade", () => {
-          this.playing = false;
-          this.howl.pause(this.id);
-        }, this.id);
-      }
-    }
-  }
-
-  // Ambient bird noises for morning endings
-  const audioRootPath = "assets/sound/";
-
+/*
+Simple sound controller using howler.js
+*/
+class SoundInstance {
   /**
    *
    * @param {Howl} howlObject
-   * @param  {...((passage: Passage) => boolean)} predicates
-   * @returns {number}
    */
-  const passageAudio = (howlObject, ...predicates) => {
-    const soundInstance = new SoundInstance(howlObject);
-    $(window).on("sm.passage.shown", (ev, { passage }) => {
-      if (predicates.some(f => f(passage))) {
-        soundInstance.fadeIn();
-      } else {
-        soundInstance.fadeOut();
-      }
-    })
-    return soundInstance.id;
+  constructor(howlObject) {
+    this.howl = howlObject;
+    this.playing = false;
+  }
+  fadeIn(duration = 1000) {
+    // If sound isn't playing yet, then fade in.
+    if (this.playing) {
+      return;
+    }
+    if (this.id == undefined) {
+      this.howl.load();
+      this.id = this.howl.play();
+    } else {
+      this.howl.play(this.id);
+    }
+    this.howl.fade(0.0, this.howl.volume(), duration, this.id);
+    this.playing = true;
+    this.howl.off("fade", this.id);
+  }
+  fadeOut(duration = 1000) {
+    if (this.howl.playing(this.id)) {
+      this.howl.fade(this.howl.volume(), 0.0, duration, this.id);
+      this.playing = false;
+      this.howl.once("fade", () => {
+        this.playing = false;
+        this.howl.pause(this.id);
+      }, this.id);
+    }
   }
 
-  /**
-   *
-   * @param  {...any} tags
-   * @returns {(passage: Passage) => boolean}
-   */
-  const hasTags = (...tags) => (passage) => tags.every(t => passage.tags.includes(t));
 
-  const howlDefaults = {
-    loop: true,
-    volume: 0.1,
-    autoplay: false,
-    preload: false
-  }
-  // Chirping birds from freesound
-  // Used in the "morning after" passages
-  const birds = new Howl({
-    ...howlDefaults,
-    src: ["assets/sound/birds.mp3"],
-    volume: 0.05,
-  });
-  passageAudio(birds, hasTags("morning"));
-  // Stomach sounds from freesound
-  const gurgle = new Howl({
-    ...howlDefaults,
-    src: ["assets/sound/gurgle.mp3"],
-    volume: 1.0,
-  });
-  passageAudio(gurgle, hasTags("vore"));
-  // Crickets and suburban outdoor noises from freesound
-  const crickets = new Howl({
-    ...howlDefaults,
-    src: ["assets/sound/crickets.mp3"],
-    volume: 0.1,
-  });
-  passageAudio(crickets, hasTags("outside"));
-  // Ambient noise by CD_YIN_001.mp3 by kevp888
-  // Used in the zen scene
-  const zenMusic = new Howl({
-    ...howlDefaults,
-    src: ["assets/sound/kevp888 - CD_YIN_001.mp3"],
-    volume: 1.0,
-  });
-  passageAudio(zenMusic, hasTags("zen"));
 }
 
-setup.playAchievementSound = () => {
-  const pikoop = new Howl({
-    src: ["assets/sound/xbox-360-achievement-sound.mp3"],
-    autoplay: true,
+const SOUNDS_PATH = "assets/sound/";
+const MUSIC_PATH = "assets/music/";
+
+/**
+ * On each passage load, checks if the predicate returns true, and the
+ * either fade in or out this sound.
+ * 
+ * @param {Howl} howlObject The sound to use.
+ * @param  {...string} tags
+ * @returns {number}
+ */
+const ambienceOnPassageTag = (howlObject, ...tags) => {
+  const soundInstance = new SoundInstance(howlObject);
+  $(window).on("sm.passage.shown", (_, { passage }) => {
+    if (tags.some(t => passage.tags.includes(t))) {
+      soundInstance.fadeIn();
+    } else {
+      soundInstance.fadeOut();
+    }
+  })
+  return soundInstance.id;
+}
+
+const howlDefaults = {
+  loop: true,
+  volume: 0.1,
+}
+
+const AmbientSounds = window.AmbientSounds = {
+  // Chirping birds from freesound
+  // Used in the "morning after" passages
+  birds: new Howl({
+    ...howlDefaults,
+    src: [`${SOUNDS_PATH}/birds.mp3`],
+    volume: 0.05,
+    loop: true
+  }),
+  // Stomach sounds from freesound
+  // Crickets and suburban outdoor noises from freesound
+  gurgle: new Howl({
+    ...howlDefaults,
+    src: [`${SOUNDS_PATH}/gurgle.mp3`],
+    volume: 1.0,
+    loop: true
+  }),
+  crickets: new Howl({
+    ...howlDefaults,
+    src: [`${SOUNDS_PATH}/crickets.mp3`],
+    volume: 0.1,
+    loop: true
+  }),
+  zenMusic: new Howl({
+    ...howlDefaults,
+    src: [`${MUSIC_PATH}/Vylet Pony - Super Pony World- Fairytails - 18 Dance of the Macabre.mp3`],
+    volume: 1.0,
+    loop: true
+  }),
+  crowdChatterClose: new Howl({
+    ...howlDefaults,
+    src: [`${SOUNDS_PATH}/chatter-close.mp3`],
+    volume: 0.2,
+    loop: true
+  }),
+  crowdChatterQuiet: new Howl({
+    ...howlDefaults,
+    src: [`${SOUNDS_PATH}/chatter-far.mp3`],
+    volume: 0.1,
+    loop: true
+  }),
+  dubstep: new Howl({
+    ...howlDefaults,
+    src: [`${MUSIC_PATH}/Vylet Pony - Invasion (ft. L.M.).mp3`],
+    volume: 0.2,
+    loop: true
+  }),
+};
+
+const SoundEffects = window.SoundEffects = {
+  achievementGet: new Howl({
+    src: [`${SOUNDS_PATH}/xbox-360-achievement-sound.mp3`],
     volume: 0.6,
   })
+};
 
-  pikoop.play();
+// ambienceOnPassageTag(AmbientSounds.title, "menu");
+ambienceOnPassageTag(AmbientSounds.birds, "morning");
+ambienceOnPassageTag(AmbientSounds.gurgle, "vore");
+ambienceOnPassageTag(AmbientSounds.crickets, "outside");
+ambienceOnPassageTag(AmbientSounds.zenMusic, "zen");
+ambienceOnPassageTag(AmbientSounds.crowdChatterClose, "party", "dubstep");
+ambienceOnPassageTag(AmbientSounds.crowdChatterQuiet, "party_far", "drunk");
+ambienceOnPassageTag(AmbientSounds.dubstep, "dubstep");
+
+
+setup.playAchievementSound = () => {
+  SoundEffects.achievementGet.play();
 }
